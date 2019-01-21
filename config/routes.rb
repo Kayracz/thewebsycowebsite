25064ef1c8058982c157ec16c9cda576c311d008
Kayra Czerniewicz <kayracz@gmail.com>
2019-01-21 16:48:06 -0400

    Setup of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/copyright'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'home#index'
 end
