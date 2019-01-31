f0b99ba514d13dcaa7a13c25f9cfc91231f0f8f8
Kayra Czerniewicz <kayracz@gmail.com>
2019-01-31 13:14:16 -0400

    emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
-  attribute :email
-  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :file,      :attachment => true
+
   attribute :message
+  attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
@@ -11,6 +13,19 @@ class Contact < MailForm::Base
       :subject => "My Contact Form",
       :to => "kayracz@gmail.com",
       :from => %("#{name}" <#{email}>)
+    }
+  end
+end
+
+include MailForm::Delivery
+
+  append :remote_ip, :user_agent, :session
+  attributes :name, :email, :created_at
+
+  def headers
+    {
+      :to => "kayracz@gmail.com",
+      :subject => "User created an account"
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'home/contact'
   get 'home/copyright'
   resources :contacts
-  resources 'contacts', only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'home#index'
 end
