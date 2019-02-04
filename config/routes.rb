3f35f52ee1173db74974cc1c454b0120bd986bd3
Kayra Czerniewicz <kayracz@gmail.com>
2019-02-04 17:05:14 -0400

    devise bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :contacts
   root to: 'home#index'
 
   get 'home/index'
   get 'home/about'
   get 'home/contact'
   get 'home/copyright'
+
+  get 'contacts/new'
+  get 'contacts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
