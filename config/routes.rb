c347bd2ffc63552a0ab80f4f4ed1ca1c037d9fe4
Kayra Czerniewicz <kayracz@gmail.com>
2019-02-05 19:20:13 -0400

    new try with index in app messages

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   get 'contacts/new'
   get 'contacts/show'
+  get 'contacts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
