8d0a25bddec4c617d8004f9ff75e82bb60079240
Kayra Czerniewicz <kayracz@gmail.com>
2019-04-01 20:42:06 -0400

    blog new html structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,42 +14,61 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-$(document).ready(function() {
-    // Configure/customize these variables.
-    var showChar = 400;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "Show more";
-    var lesstext = "Show less";
-
-
-    $('.more').each(function() {
-        var content = $(this).html();
-
-        if(content.length > showChar) {
-
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
-
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-
-            $(this).html(html);
-        }
-
-    });
-
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
+$('[data-show="more"]').on('click', function(event) {
+ event.preventDefault();
+    if ( $(this).attr('more-collapse') === 'false' ) {
+     $(this).attr('more-collapse', 'true');
+     $(this).prev('.more-text').removeClass('hide');
+     $(this).text('less');
+    }  else {
+         $(this).text('more');
+         $(this).attr('more-collapse', 'false');
+         $(this).prev('.more-text').addClass('hide');
+    }
 });
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     // Configure/customize these variables.
+//     var showChar = 400;  // How many characters are shown by default
+//     var ellipsestext = "...";
+//     var moretext = "Show more";
+//     var lesstext = "Show less";
+
+
+//     $('.more').each(function() {
+//         var content = $(this).html();
+
+//         if(content.length > showChar) {
+
+//             var c = content.substr(0, showChar);
+//             var h = content.substr(showChar, content.length - showChar);
+
+//             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+//             $(this).html(html);
+//         }
+
+//     });
+
+//     $(".morelink").click(function(){
+//         if($(this).hasClass("less")) {
+//             $(this).removeClass("less");
+//             $(this).html(moretext);
+//         } else {
+//             $(this).addClass("less");
+//             $(this).html(lesstext);
+//         }
+//         $(this).parent().prev().toggle();
+//         $(this).prev().toggle();
+//         return false;
+//     });
+// });
 
 
 
